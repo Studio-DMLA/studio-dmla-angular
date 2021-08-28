70f0a9461531351e99ed4d12cdc3823591e3b398
Robert <rleeuwerink@gmail.com>
2021-08-28 15:19:43 -0700

    project updates

--- src/app/ui/page/pages-data.ts
+++ src/app/ui/page/pages-data.ts
@@ -69,18 +69,30 @@ export const pages: Pages = {
     },
     content: `<p>Check out the latest projects we have been working on:</p>
     <div class="portfolio-items">
-    <div class="portfolio-card" >
-    <h3><span>2021 Global Village Hawaii Fee Sheet:</span></h3>
-      <a href="https://lnkd.in/gqXYtsK" target="_blank">
-        <div class="img"><img src="/assets/portfolio/hawaii fee sheet.jpg" /></div>
-      </a>
-    </div>
-    <div class="portfolio-card" >
-    <h3><span>2021 Global Village Hawaii Brochure:</span></h3>
-      <a href="https://lnkd.in/gsZKx9F" target="_blank">
-        <div class="img"><img src="/assets/portfolio/hawaii.jpg" /></div>
-      </a>
-    </div>
+      <div class="portfolio-card" >
+      <h3><span>2021 Global Village Hawaii Fee Sheet:</span></h3>
+        <a href="https://lnkd.in/gqXYtsK" target="_blank">
+          <div class="img"><img src="/assets/portfolio/hawaii fee sheet.jpg" /></div>
+        </a>
+      </div>
+      <div class="portfolio-card" >
+      <h3><span>2021 Global Village Hawaii Brochure:</span></h3>
+        <a href="https://lnkd.in/gsZKx9F" target="_blank">
+          <div class="img"><img src="/assets/portfolio/hawaii.jpg" /></div>
+        </a>
+      </div>
+      <div class="portfolio-card" >
+      <h3><span>Global Village Victoria Website:</span></h3>
+        <a href="https://gvenglish.com/canada" target="_blank">
+          <div class="img"><img src="/assets/portfolio/gvvic.jpg" /></div>
+        </a>
+      </div>
+      <div class="portfolio-card" >
+      <h3><span>Tribe Management:</span></h3>
+        <a href="https://tribemgmt.com" target="_blank">
+          <div class="img"><img src="/assets/portfolio/tribe.jpg" /></div>
+        </a>
+      </div>
     </div>`,
   },
   services: {
